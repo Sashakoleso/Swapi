ea497922af04264be8f46b0aa41e329e30aaeca3
SashaKoleso <skolisetsky@yahoo.com>
2020-02-11 15:03:20 +0200

    Updates

---
+++ precache-manifest.1525f8457899b37440ef4256fea62433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc591cf341786b8fe8279002f5ff42c",
+    "url": "/swapi/index.html"
+  },
+  {
+    "revision": "3772fec5c2720ba87d06",
+    "url": "/swapi/static/css/main.d8f19221.chunk.css"
+  },
+  {
+    "revision": "b37e603cfd761d78d28a",
+    "url": "/swapi/static/js/2.d2044823.chunk.js"
+  },
+  {
+    "revision": "3772fec5c2720ba87d06",
+    "url": "/swapi/static/js/main.0b7eee51.chunk.js"
+  },
+  {
+    "revision": "474dacd52c8fcc617716",
+    "url": "/swapi/static/js/runtime-main.5f5cde36.js"
+  },
+  {
+    "revision": "0a68e6599f5504ded0634868790d90a4",
+    "url": "/swapi/static/media/wp2902991.0a68e659.jpg"
+  }
+]);
